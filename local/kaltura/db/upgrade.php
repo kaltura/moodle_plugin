95b68c10eb721916b6acacf92205bfa2f8f3244a
Muli Dayan <muli.dayan@kaltura.com>
2016-04-05 09:12:18 +0300

    KMS-11303 increment version to 2016050429

--- local/kaltura/db/upgrade.php
+++ local/kaltura/db/upgrade.php
@@ -59,13 +59,13 @@ function xmldb_local_kaltura_upgrade($oldversion) {
         }
 
         // Kaltura savepoint reached.
-        upgrade_plugin_savepoint(true, 2015120909, 'local', 'kaltura');
+        upgrade_plugin_savepoint(true, 2016040529, 'local', 'kaltura');
         $savePointDone = true;
     }
 
-    if (!$savePointDone && $oldversion < 2015120909) {
+    if (!$savePointDone && $oldversion < 2016040529) {
         // Kaltura savepoint reached.
-        upgrade_plugin_savepoint(true, 2015120909, 'local', 'kaltura');
+        upgrade_plugin_savepoint(true, 2016040529, 'local', 'kaltura');
     }
     return true;
 }
