f555b9aaad4c32e74bd5a2b729e093f41e0d13cb
Muli Dayan <muli.dayan@kaltura.com>
2017-08-14 11:59:51 +0300

    increment version - moodle 3.2

--- blocks/kalturamediagallery/version.php
+++ blocks/kalturamediagallery/version.php
@@ -14,11 +14,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017062532;
-$plugin->release = 'Kaltura release 4.1';
+$plugin->version   = 2017081432;
+$plugin->release = 'Kaltura release 4.1.1';
 $plugin->requires  = 2014050800;
 $plugin->component = 'block_kalturamediagallery';
 $plugin->dependencies = array(
-    'local_kaltura' => 2017062532,
-    'local_kalturamediagallery' => 2017062532
+    'local_kaltura' => 2017081432,
+    'local_kalturamediagallery' => 2017081432
 );
