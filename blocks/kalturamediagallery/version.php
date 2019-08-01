53acfbf54f42396ef55a72cb81c40ef6a7ea6730
Noy Cohen <noy.cohen@kaltura.com>
2019-08-01 12:08:52 +0300

    version bump - 4.2.3

--- blocks/kalturamediagallery/version.php
+++ blocks/kalturamediagallery/version.php
@@ -14,11 +14,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019072536;
-$plugin->release = 'Kaltura release 4.2.2';
+$plugin->version   = 2019080136;
+$plugin->release = 'Kaltura release 4.2.3';
 $plugin->requires  = 2018120300;
 $plugin->component = 'block_kalturamediagallery';
 $plugin->dependencies = array(
-    'local_kaltura' => 2019072536,
-    'local_kalturamediagallery' => 2019072536
+    'local_kaltura' => 2019080136,
+    'local_kalturamediagallery' => 2019080136
 );
