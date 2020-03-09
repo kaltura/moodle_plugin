fc564dd926680b76e9a6acf6b398f4f807704d54
Noy Cohen <noy.cohen@kaltura.com>
2020-03-09 14:26:13 +0200

    version bump - 4.2.7

--- blocks/kalturamediagallery/version.php
+++ blocks/kalturamediagallery/version.php
@@ -14,11 +14,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020010537;
-$plugin->release = 'Kaltura release 4.2.6';
+$plugin->version   = 2020030937;
+$plugin->release = 'Kaltura release 4.2.7';
 $plugin->requires  = 2018120300;
 $plugin->component = 'block_kalturamediagallery';
 $plugin->dependencies = array(
-    'local_kaltura' => 2020010537,
-    'local_kalturamediagallery' => 2020010537
+    'local_kaltura' => 2020030937,
+    'local_kalturamediagallery' => 2020030937
 );
