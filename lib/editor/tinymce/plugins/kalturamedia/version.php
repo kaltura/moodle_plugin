af7b1a309f167219a2650dfa0168b46bb3b82194
Noy Cohen <noy.cohen@kaltura.com>
2021-11-16 16:00:04 +0200

    KMS-24186 update version to 4.3.1

--- lib/editor/tinymce/plugins/kalturamedia/version.php
+++ lib/editor/tinymce/plugins/kalturamedia/version.php
@@ -23,12 +23,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021062039;
-$plugin->release = 'Kaltura release 4.3.0';
+$plugin->version   = 2020061501;
+$plugin->release = 'Kaltura release 4.3.1';
 // Required Moodle version.
 $plugin->requires  = 2018120300;
 // Full name of the plugin (used for diagnostics).
 $plugin->component = 'tinymce_kalturamedia';
 $plugin->dependencies = array(
-    'local_kaltura' => 2021062039
+    'local_kaltura' => 2020061501
 );
