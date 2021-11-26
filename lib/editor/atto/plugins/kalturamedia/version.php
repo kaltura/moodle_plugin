562c3b14b6bbeadb785a25f8e1899ea6f1093899
Will.Wise <ww2294@open.ac.uk>
2021-11-26 10:20:38 +0000

    Atto: support showing button by capability

--- lib/editor/atto/plugins/kalturamedia/version.php
+++ lib/editor/atto/plugins/kalturamedia/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021051701;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021051702;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release = 'Kaltura release 4.3.1';
 $plugin->requires  = 2018120300;        // Requires this Moodle version.
 $plugin->component = 'atto_kalturamedia';  // Full name of the plugin (used for diagnostics).
