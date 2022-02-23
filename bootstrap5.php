33535034083ba547a63f7ff2b31c3b1ac47940f4
Joel Dapiawen <joel.dapiawen@uregina.ca>
2022-02-23 16:51:33 -0600

    added bootsrap 5 to different file to be able to re-used it.

---
+++ bootstrap5.php
@@ -0,0 +1,40 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+
+<style>
+  .accordion-button:not(.collapsed) {
+    color: inherit;
+    background: rgb(77, 77, 79);
+    color: #e8e8e8;
+  }
+  .accordion-button {
+    background: #d7d7d7;
+  }
+  .accordion-item {
+ border: 1px solid rgba(35, 65, 100, .15) !important;
+  }
+  .accordion-button:not(.collapsed)::after {
+   filter: brightness(0%) invert(80%);
+  }
+  .accordion-button:focus{
+    box-shadow: inherit;
+  }
+  .accordion-border {
+    border: 1px solid rgba(35, 65, 100, .1) !important;
+  }
+
+  </style>
+</head>
