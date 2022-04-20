3b8272696a76880c11b65897733b5b916de9798b
hilak <hila.karimov@kaltura.com>
2022-04-20 16:02:30 -0400

    V4.3.2 for Moodle 3.10 - support screenshare

--- lib/editor/atto/plugins/kalturamedia/version.php
+++ lib/editor/atto/plugins/kalturamedia/version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020110901;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = 'Kaltura release 4.3.1';
+$plugin->version   = 2020110902;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = 'Kaltura release 4.3.2';
 $plugin->requires  = 2018120300;        // Requires this Moodle version.
 $plugin->component = 'atto_kalturamedia';  // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array(
-    'local_kaltura' => 2020110901
+    'local_kaltura' => 2020110902
 );
--- lib/editor/tinymce/plugins/kalturamedia/version.php
+++ lib/editor/tinymce/plugins/kalturamedia/version.php
@@ -23,12 +23,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020110901;
-$plugin->release = 'Kaltura release 4.3.1';
+$plugin->version   = 2020110902;
+$plugin->release = 'Kaltura release 4.3.2';
 // Required Moodle version.
 $plugin->requires  = 2018120300;
 // Full name of the plugin (used for diagnostics).
 $plugin->component = 'tinymce_kalturamedia';
 $plugin->dependencies = array(
-    'local_kaltura' => 2020110901
+    'local_kaltura' => 2020110902
 );
