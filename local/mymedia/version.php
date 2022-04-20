3c935a232950e5c2d54b50734427f98af66c4f9b
hilak <hila.karimov@kaltura.com>
2022-04-20 16:55:40 -0400

    V4.3.2 for Moodle 3.9 - support screenshare

--- local/mymedia/version.php
+++ local/mymedia/version.php
@@ -25,11 +25,11 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version = 2020061501;
+$plugin->version = 2020061502;
 $plugin->component = 'local_mymedia';
-$plugin->release = 'Kaltura release 4.3.1';
+$plugin->release = 'Kaltura release 4.3.2';
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'local_kaltura' => 2020061501
+    'local_kaltura' => 2020061502
 );
