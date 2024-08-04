c152609efa8669b903fa09e11f33c1c9e2080ded
Luca Bösch <luca.boesch@bfh.ch>
2024-08-04 14:28:30 +0200

    Remove deprecated get_legacy_logdata functions.

--- mod/kalvidassign/classes/event/assignment_submitted.php
+++ mod/kalvidassign/classes/event/assignment_submitted.php
@@ -51,9 +51,4 @@ class assignment_submitted extends \core\event\base {
     public function get_url() {
         return new \moodle_url('/mod/kalvidassign/view.php', array('cmid' => $this->contextinstanceid));
     }
- 
-    public function get_legacy_logdata() {
-        return array($this->courseid, 'kalvidassign', 'submit',
-            $this->get_url(), $this->objectid, $this->contextinstanceid);
-    }
 }
