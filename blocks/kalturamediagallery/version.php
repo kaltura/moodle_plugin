b846222ea0c6c217e09e789972676de13a14ccf1
roileviK <roi.levi@kaltura.com>
2025-07-22 14:03:48 +0300

    SUP-42220 bump plugin version

--- blocks/kalturamediagallery/version.php
+++ blocks/kalturamediagallery/version.php
@@ -14,11 +14,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023100903;
-$plugin->release = 'Kaltura release 4.5.0';
+$plugin->version   = 2023100904;
+$plugin->release = 'Kaltura release 4.5.1';
 $plugin->requires  = 2023100900;
 $plugin->component = 'block_kalturamediagallery';
 $plugin->dependencies = array(
-    'local_kaltura' => 2023100903,
-    'local_kalturamediagallery' => 2023100903
+    'local_kaltura' => 2023100904,
+    'local_kalturamediagallery' => 2023100904
 );
--- local/mymedia/version.php
+++ local/mymedia/version.php
@@ -25,11 +25,11 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version = 2023100903;
+$plugin->version = 2023100904;
 $plugin->component = 'local_mymedia';
-$plugin->release = 'Kaltura release 4.5.0';
+$plugin->release = 'Kaltura release 4.5.1';
 $plugin->requires = 2023100900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'local_kaltura' => 2023100903
+    'local_kaltura' => 2023100904
 );
