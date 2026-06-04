a44ac7be614b2ea9ccfb98326d23146e61169090
roileviK <roi.levi@kaltura.com>
2026-06-04 12:31:23 +0300

    SUP-52213 add local-network-access iframe allow attribute

--- mod/kalvidassign/single_submission_form.php
+++ mod/kalvidassign/single_submission_form.php
@@ -70,7 +70,7 @@ class kalvidassign_singlesubmission_form extends moodleform {
                 'height' => $submission->height,
                 'width' => $submission->width,
                 'allowfullscreen' => 'true',
-                'allow' => 'autoplay *; fullscreen *; encrypted-media *; camera *; microphone *; display-capture *; clipboard-write *;',
+                'allow' => 'autoplay *; fullscreen *; encrypted-media *; camera *; microphone *; display-capture *; clipboard-write *; local-network-access *;',
             );
             $mform->addElement('html', html_writer::tag('iframe', '', $attr));
         }
