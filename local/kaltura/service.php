9fed2a0e49ca44661c5e51c2d143e99e1fee6aa2
roileviK <roi.levi@kaltura.com>
2026-06-08 16:06:33 +0300

    KMS-33419 replace require_login() with NO_MOODLE_COOKIES to fix moodle session logout in cross-site navigation with KAF

--- local/kaltura/service.php
+++ local/kaltura/service.php
@@ -23,10 +23,10 @@
  * @copyright  (C) 2014 Remote Learner.net Inc http://www.remote-learner.net
  */
 
+define('NO_MOODLE_COOKIES', true);
+
 require_once(dirname(__FILE__).'/../../config.php');
 require_once($CFG->dirroot.'/local/kaltura/locallib.php');
-
-require_login();
 
 global $PAGE;
 
