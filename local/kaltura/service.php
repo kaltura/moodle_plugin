028bf946287ca4218c33df79b0a515880bd5c196
roileviK <roi.levi@kaltura.com>
2026-06-08 17:19:05 +0300

    KMS-33419 revert isloggedin check

--- local/kaltura/service.php
+++ local/kaltura/service.php
@@ -56,7 +56,7 @@ $serviceurl = new moodle_url('/local/kaltura/service.php');
 
 // Log the request.
 $enablelogging = get_config(KALTURA_PLUGIN_NAME, 'enable_logging');
-if (!empty($enablelogging) && isloggedin()) {
+if (!empty($enablelogging)) {
     $param = array(
         'url' => $url,
         'width' => $width,
