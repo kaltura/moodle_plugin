24420cc7f4140d6bf94fb4dc1e286a83c08179f7
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2026-08-30 09:35:21 +0200

    Add Italian language strings for Kaltura gallery block

---
+++ blocks/kalturamediagallery/lang/it/block_kalturamediagallery.php
@@ -0,0 +1,20 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['pluginname'] = 'Galleria multimediale Kaltura';
+$string['kalturamediagallery:addinstance'] = 'Aggiungere un nuovo blocco Galleria multimediale Kaltura';
+$string['kalturamediagallery:myaddinstance'] = 'Aggiungere un nuovo blocco Galleria multimediale Kaltura alla Dashboard';
+$string['privacy:metadata'] = 'Il blocco Galleria multimediale Kaltura mostra solo un collegamento al plugin locale della galleria multimediale Kaltura.';
