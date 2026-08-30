d63c9762933571725004685e36f710841acef285
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2026-08-30 09:23:21 +0200

    Add Italian language file for My Media plugin
    This file contains Italian language strings for the My Media plugin in Moodle, including headings, error messages, and settings descriptions.

---
+++ local/mymedia/lang/it/local_mymedia.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * My Media language file.
+ *
+ * @package    local_mymedia
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  (C) 2014 Remote Learner.net Inc http://www.remote-learner.net
+ */
+
+$string['heading_mymedia'] = 'I miei media';
+$string['invalid_launch_parameters'] = 'Parametri di avvio non validi';
+$string['mymedia:view'] = 'Visualizzare la pagina I miei media';
+$string['nav_mymedia'] = 'I miei media';
+$string['pluginname'] = 'I miei media';
+$string['setting_heading_desc'] = 'Impostazioni';
+$string['link_location'] = 'Posizione del collegamento';
+$string['link_location_desc'] = 'Scegli dove visualizzare il collegamento a I miei media';
+$string['link_location_top_menu'] = 'Menu di navigazione superiore';
+$string['link_location_side_menu'] = 'Menu di navigazione laterale';
+$string['link_location_user_menu'] = 'Menu di navigazione utente';
+$string['privacy:metadata'] = 'Il plugin I miei media non memorizza alcun dato personale.';
